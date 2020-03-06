1370ff957cf5f53a8c9b621676c7352517083c0e
fchiapini <felipe.chiapini@gmail.com>
2020-03-06 13:54:46 +0900

    Income and Expense components

--- client/src/components/Expense.vue
+++ client/src/components/Expense.vue
@@ -1,12 +1,12 @@
 <template>
     <v-data-table
       :headers="headers"
-      :items="incomes"
+      :items="expenses"
       sort-by="date"
       class="elevation-1" >
       <template v-slot:top >
         <v-toolbar flat color="white">
-          <v-toolbar-title>Income</v-toolbar-title>
+          <v-toolbar-title>Expense</v-toolbar-title>
           <v-divider
             class="mx-4"
             inset
@@ -15,7 +15,7 @@
           <v-spacer></v-spacer>
           <v-dialog v-model="dialog" max-width="1000px">
             <template v-slot:activator="{ on }">
-              <v-btn color="primary" dark class="mb-2" v-on="on">New Income</v-btn>
+              <v-btn color="primary" dark class="mb-2" v-on="on">New Expense</v-btn>
             </template>
             <v-card>
             <v-card-title>
@@ -29,7 +29,14 @@
                     <v-select
                       v-model="editedItem.category"
                       :items="categories"
-                      label="Category income"
+                      label="Category expense"
+                    ></v-select>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-select
+                      v-model="editedItem.description"
+                      :items="descriptions"
+                      label="Description"
                     ></v-select>
                   </v-col>
                   <v-col cols="12" sm="6" md="4">
@@ -74,7 +81,7 @@
 
 <script>
 export default {
-    name: 'Income',
+    name: 'Expense',
 
     data: () => ({
       dialog: false,
@@ -86,27 +93,39 @@ export default {
           value: 'category'
         },
         { text: 'Amount', value: 'amount', sortable: false },
+        { text: 'Description', value: 'amount', sortable: false },
         { text: 'Date', value: 'date', sortable: true },
         { text: 'Actions', value: 'action', sortable: false }
       ],
-      incomes: [],
+      expenses: [],
       editedIndex: -1,
       editedItem: {
         category: '',
         amount: 0,
+        description: '',
         date: null
       },
       defaultItem: {
         category: '',
         amount: 0,
+        description: '',
         date: null
       },
-      categories: ['Salary', 'Renting', 'Pension', 'Extra hours', 'Bonus', 'Vacation', 'Others']
+      categories: ['Housing', 'Transport', 'Food', 'Health', 'Education', 'Clothing', 'Personal care', 'Leisure', 'Taxes', 'Others'],
+      descriptions: [
+          'Renting', 'Housing fee', 'House instalment', 'Housing insurance', 
+          'House cleaning', 'Car instalment', 'Car insurance', 'Parking lot fee',
+          'Health insurance', 'Health care', 'School', 'College', 'Course', 
+          'Housing tax', 'Life insurance', 'Electricity', 'Water', 'Phone bill',
+          'Gas', 'Cable/Streaming', 'Internet', 'Train/Metro', 'Bus', 'Fuel', 'Supermarket',
+          'Grocery shop', 'Bakery', 'Medicine', 'Hair', 'Gym', 'Doctor', 'Dentist', 'Hospital',
+          'Trip', 'Theater', 'Restaurant/Bar', 'Movie renting', 'Cloth', 'Shoes', 'Accessories', 'Gifts'
+      ]
     }),
 
     computed: {
       formTitle() {
-        return this.editedIndex === -1 ? 'New Income' : 'Edit Income';
+        return this.editedIndex === -1 ? 'New Expense' : 'Edit Expense';
       }
     },
 
@@ -122,39 +141,30 @@ export default {
 
     methods: {
       initialize() {
-        this.incomes = [
+        this.expenses = [
           {
-            category: 'Salary',
+            category: 'Housing',
             amount: 100000,
+            description: 'Renting',
             date: '2020-03-06'
           },
           {
-            category: 'Renting',
-            amount: 100000,
-            date: '2020-03-06'
-          },
-          {
-            category: 'Vacation',
-            amount: 100000,
-            date: '2020-03-06'
-          },
-          {
-            category: 'Extra hours',
-            amount: 100000,
-            date: '2020-03-06'
-          }
+            category: 'Food',
+            amount: '30000',
+            description: 'Supermarket',
+            date: null}
         ]
       },
 
       editItem (item) {
-        this.editedIndex = this.incomes.indexOf(item);
+        this.editedIndex = this.expenses.indexOf(item);
         this.editedItem = Object.assign({}, item);
         this.dialog = true;
       },
 
       deleteItem (item) {
-        const index = this.incomes.indexOf(item);
-        confirm('Are you sure you want to delete this item?') && this.incomes.splice(index, 1);
+        const index = this.expenses.indexOf(item);
+        confirm('Are you sure you want to delete this item?') && this.expenses.splice(index, 1);
       },
 
       close () {
@@ -167,9 +177,9 @@ export default {
 
       save () {
         if (this.editedIndex > -1) {
-          Object.assign(this.incomes[this.editedIndex], this.editedItem);
+          Object.assign(this.expenses[this.editedIndex], this.editedItem);
         } else {
-          this.incomes.push(this.editedItem);
+          this.expenses.push(this.editedItem);
         }
         this.close();
       }
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Income></Income>
+    <Expense></Expense>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Income from '@/components/Income.vue';
+import Expense from '@/components/Expense.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Income,
+    Expense
   }
 }
 </script>
